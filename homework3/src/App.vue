62cf9879b1ad22b4aceb6c82996931a2f4c47e2c
Farnaz Baksh <farnaz.baksh@gmail.com>
2022-11-19 16:46:46 +0200

    template for main & signup pages

--- homework3/src/App.vue
+++ homework3/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Main</router-link> |
+    <router-link to="/signup">Signup</router-link>
   </nav>
   <router-view/>
-</template>
+</template> 
 
 <style>
 #app {
---
+++ homework3/src/views/FormView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Test page</h1>
+  </div>
+</template>
