95107dfa9aae315447f90cfc611ea77432d74c5b
Farnaz Baksh <farnaz.baksh@gmail.com>
2022-11-20 22:57:54 +0200

    working on main

--- homework3/src/store/index.js
+++ homework3/src/store/index.js
@@ -1,55 +1,56 @@
 import { createStore } from 'vuex'
 
-// inspiration from: https://medium.com/@esmaydogdu/vuex-fetch-b0e8472e7676
-
 export default createStore({
-  strict: true,
-  state: {
-    postsList:[]
-  },
+    strict: true,
+    state: {
+            // productList should be placed here
+            productList:[
+                {id: 1, author: "Charlotte Bronte", book: "Jane Eyre", goodreads: "4.1/5", price: 20},
+                {id: 2, author: "Margaret Mitchell", book: "Gone with the Wind", goodreads: "4.3/5", price: 22},
+                {id: 3, author: "Anthony Burgess", book: "A Clockwork Orange", goodreads: "4/5", price: 13},
+                {id: 4, author: "Fyodor Dostoevsky" , book: "Crime and Punishment", goodreads: "4.2/5", price: 18}
+                ]
+    },
   getters: {
     // .map is a higher-order function that creates a new array populated with the results of
     // calling a provided function on every element in the calling array. – Lecture 5
+
     productListsale: state => {
-      var likeList = state.likeList.map(product => {
-          return {
-              index: product.index,
-              author: product.author,
-              price: product.price / 2,
-              book: product.book,
-              goodreads: product.goodreads
-          }
-      });
-      return likeList
+    var productListsale = state.productList.map(product => {
+               return {
+                   id: product.id,
+                   author: product.author,
+                   price: product.price / 2,
+                   book: product.book,
+                   goodreads: product.goodreads
+               }
+           });
+           return productListsale
+       },
+      },
+    mutations: {
+           //The .forEach() method executes a callback function on each of the elements in an array in order. – Lecture 5
+           IncreasePrice: state => {
+            state.productList.forEach(product => {
+                product.price += 1;
+            })
+        },
+        DecreasePrice: state => {
+            state.productList.forEach(product => {
+                product.price -= 1;
+            })
+        }
     },
-    countries(state) {
-      return state.countries;
-    }
-  },
-  mutations: {
-    setCountries(state, countries) {
-      state.countries = countries;
-    },  
-    //The .forEach() method executes a callback function on each of the elements in an array in order. – Lecture 5
-    IncreasePrice: state => {
-      state.likeList.forEach(product => {
-      product.price += 1;
-      })
-    },
-    DecreasePrice: state => {
-      state.likeList.forEach(product => {
-        product.price -= 1;
-      })
-    }
-  },
   actions: {
-    fetchAllCountries(context) {
-      return fetch("https://api.npoint.io/054c02512e7ae401d479")
-      .then((response) => response.json())
-      .then((data) => {
-        context.commit("setCountries", data);
-      })
-      .catch((err) => console.error(err));
+    IncreasePriceAct: act => {
+        setTimeout(function() {
+            act.commit("IncreasePrice")
+        }, 1000)
     },
-  }
+    DecreasePriceAct: act => {
+        setTimeout(function() {
+            act.commit("DecreasePrice")
+        }, 1000)
+    }
+    }
 })
--- homework3/src/views/HomeView.vue
+++ homework3/src/views/HomeView.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <HelloWorld/>
+  <div class="products">
+   <br>
+   <h2> Our Products page</h2>
+   <product-compo-two></product-compo-two>
+
+   <button v-on:click="IncreasePrice "> Increase price </button>
   </div>
-</template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-
-</script>
+ </template>
+ 
+ <script>
+ 
+ import ProductCompoTwo from "@/components/ProductCompoTwo.vue";
+ 
+ export default {
+ name: "Products",
+ components: { ProductCompoTwo },
+ data: function() {
+ return {
+ }},
+ 
+ methods: {
+ IncreasePrice: function() {
+   //this.$store.commit("IncreasePrice")
+   this.$store.dispatch("IncreasePriceAct")
+ 
+     /*
+     When using the strict mode in the vuex store, we cannot mutate state outside mutation handlers.
+     Therefore, using the following function will produce an error
+     
+     this.$store.state.productList.forEach(product => {
+         product.price += 1;
+         }) 
+     */
+ },
+ DecreasePrice: function() {
+   //this.$store.commit("DecreasePrice")
+   this.$store.dispatch("DecreasePriceAct")
+ }
+ 
+ }
+ }
+ </script>
+ 
+ <style scoped>
+ button{
+ background:  rgb(31, 163, 163);
+ border: 0;
+ padding: 10px 20px 10px 20px;
+ margin-top:  20px;
+ margin-bottom:  20px;
+ margin-right:  10px;
+ color: white;
+ border-radius: 20px;
+ }
+ </style>
+ 
