2cfefe38d2c1382d4814f29681071e0e37377f23
MiaMarta <mia.martaherta@gmail.com>
2022-12-09 14:21:15 +0200

    saves only

--- homework4/back-end/database.js
+++ homework4/back-end/database.js
@@ -13,7 +13,7 @@ const Pool = require('pg').Pool;
 // this code will work and a table will be created if you have already created the "testWad" database.
 const pool = new Pool({
     user: "postgres",
-    password: "okay592", // Enter your password here
+    password: "DBA", // Enter your password here
     database: "testWad", //Try to use the same name for your database
     host: "localhost",
     port: "5432"
